d4038e0642a3b69a176121902115544f88fc311c
soeun <separk314@naver.com>
2023-09-24 23:54:12 +0900

    Chore: 프로젝트 세팅

---
+++ lookAtMyPet/src/main/java/lookAtMyPet/LookAtMyPetApplication.java
@@ -0,0 +1,13 @@
+package lookAtMyPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookAtMyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookAtMyPetApplication.class, args);
+	}
+
+}
---
+++ lookAtMyPet/src/test/java/lookAtMyPet/LookAtMyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package lookAtMyPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookAtMyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
